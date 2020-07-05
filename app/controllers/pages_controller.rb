f540a52af35f6913c23591511850206a9f625840
Vincent Katz <vinz.k@live.fr>
2020-07-05 16:33:43 +0300

    starting

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def job
+  end
+
+  def photography
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'job', to: 'pages#job'
+  get 'photography', to: 'pages#photography'
 end
